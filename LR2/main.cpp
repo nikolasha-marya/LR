cc013a0773e4d55ee9de5a78897c353f0b7bc87f
Maria <nikolasha.marya@yandex.ru>
2021-10-04 20:39:34 +0300

    Uр

--- LR2/main.cpp
+++ LR2/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    double x1; double x2; double y1; double y2;
+    cout << "Координаты первой точки: ";
+    cin >> x1 >> y1;
+    cout << "Координаты второй точки: ";
+    cin >> x2 >> y2;
+    double x1x2 = x2 - x1; double y1y2 = y2 - y1;
+    double AB = pow(x1x2, 2) + pow(y1y2, 2);
+    cout << "Расстояние между точками: " << sqrt(AB);
     return 0;
 }
