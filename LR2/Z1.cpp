d024d52a08bdbba62386845aac6ddeb3f3ff1157
Maria <nikolasha.marya@yandex.ru>
2021-10-04 21:40:35 +0300

    Uррjрh

---
+++ LR2/Z1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x1; double x2; double y1; double y2;
+    cout << "Координаты первой точки: ";
+    cin >> x1 >> y1;
+    cout << "Координаты второй точки: ";
+    cin >> x2 >> y2;
+    double x1x2 = x2 - x1; double y1y2 = y2 - y1;
+    double AB = pow(x1x2, 2) + pow(y1y2, 2);
+    cout << "Длина отрезка АВ: " << sqrt(AB);
+    return 0;
+}
