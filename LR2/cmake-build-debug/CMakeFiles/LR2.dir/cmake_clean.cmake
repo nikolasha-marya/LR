0e8dc1b62a7ce367dbd7c46bf76dda18f06e91ef
Maria <nikolasha.marya@yandex.ru>
2021-10-04 21:14:04 +0300

    Uррj

---
+++ LR2/cmake-build-debug/CMakeFiles/LR2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LR2.dir/Z2.cpp.obj"
+  "LR2.exe"
+  "LR2.exe.manifest"
+  "LR2.pdb"
+  "libLR2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LR2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
